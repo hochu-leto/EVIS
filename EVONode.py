f56c3579fb4d33f7254028b5705fcc3cf5c7a873
Timofey Inozemcev <timofey.inozemtsev@evocargo.com>
2022-09-10 14:21:09 +0300

    добавил пустые объекты

---
+++ EVONode.py
@@ -0,0 +1,24 @@
+class EVONode:
+    def __init__(self):
+        pass
+
+    def get_serial_number(self):
+        pass
+
+    def get_firmware_version(self):
+        pass
+
+    def get_parametr(self, address=0, name_parametr=''):
+        pass
+
+    def change_parametr(self, address=0, name_parametr=''):
+        pass
+
+    def check_errors(self):
+        pass
+
+    def erase_errors(self):
+        pass
+
+    def is_connected(self):  # возможно, это нужно сделать просто полем
+        pass
---
+++ Parametr.py
@@ -0,0 +1,9 @@
+class Parametr:
+    def __init__(self, param: dict):
+        pass
+
+    def get_value(self):
+        pass
+
+    def set_value(self):
+        pass
